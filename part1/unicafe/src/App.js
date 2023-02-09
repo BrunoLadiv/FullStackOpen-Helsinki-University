7afcf330b6c25f89b8b9f6a8e0812c22bce85bd6
Bruno Vidal <brunokasprik@gmail.com>
2023-02-09 01:28:19 -0300

    exerc 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,28 +1,29 @@
 import { useState } from 'react'
 
-
-
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const percentage = (good / total) * 100;
+  
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood((prevGood) => prevGood + 1)}>
-        good
-      </button>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood((prevGood) => prevGood + 1)}>Good</button>
       <button onClick={() => setNeutral((prevNeutral) => prevNeutral + 1)}>
-        neutral
+        Neutral
       </button>
-      <button onClick={() => setBad((prevBad) => prevBad + 1)}>
-        bad
-      </button>
-      <h1>statistics</h1>
-      <h5>good: {good}</h5>
-      <h5>neutral: {neutral}</h5>
-      <h5>bad: {bad}</h5>
+      <button onClick={() => setBad((prevBad) => prevBad + 1)}>Bad</button>
+      <h1>Statistics</h1>
+      <h5>Good: {good}</h5>
+      <h5>Neutral: {neutral}</h5>
+      <h5>Bad: {bad}</h5>
+      <h5>Total: {total}</h5>
+      <h5>Average: {average ? average.toFixed(3): 0}</h5>
+      <h5>Positive: {percentage ? percentage.toFixed(2): 0}%</h5>
     </div>
   )
 }
