954807143b8049047c4708b9a7a7131b52f06b8c
Bruno Vidal <brunokasprik@gmail.com>
2023-02-09 01:50:28 -0300

    exerc 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const percentage = (good / total) * 100
+  return (
+    <section id='statistics'>
+      <h1>Statistics</h1>
+      <h5>Good: {good}</h5>
+      <h5>Neutral: {neutral}</h5>
+      <h5>Bad: {bad}</h5>
+      <h5>Total: {total}</h5>
+      <h5>Average: {average ? average.toFixed(3) : 0}</h5>
+      <h5>Positive: {percentage ? percentage.toFixed(2) : 0}%</h5>
+    </section>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad;
-  const average = (good - bad) / total;
-  const percentage = (good / total) * 100;
-  
 
   return (
     <div>
@@ -17,13 +30,11 @@ const App = () => {
         Neutral
       </button>
       <button onClick={() => setBad((prevBad) => prevBad + 1)}>Bad</button>
-      <h1>Statistics</h1>
-      <h5>Good: {good}</h5>
-      <h5>Neutral: {neutral}</h5>
-      <h5>Bad: {bad}</h5>
-      <h5>Total: {total}</h5>
-      <h5>Average: {average ? average.toFixed(3): 0}</h5>
-      <h5>Positive: {percentage ? percentage.toFixed(2): 0}%</h5>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   )
 }
