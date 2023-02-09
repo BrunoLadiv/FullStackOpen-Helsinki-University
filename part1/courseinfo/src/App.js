9a3e91b5a0eff12e016eef9e675abc3057e8eb1b
Bruno Vidal <brunokasprik@gmail.com>
2023-02-08 21:18:04 -0300

    exerc 1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,11 +2,21 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>
 }
 const Content = (props) => {
+  console.log(props)
   return (
     <>
-      <Part part={props.part1}  exercise={props.exercise1}/>
-      <Part part={props.part2} exercise={props.exercise2}/>
-      <Part part={props.part3} exercise={props.exercise3}/>
+      <Part
+        part={props.part1.name}
+        exercise={props.part1.exercises}
+      />
+      <Part
+        part={props.part2.name}
+        exercise={props.part2.exercises}
+      />
+      <Part
+        part={props.part3.name}
+        exercise={props.part3.exercises}
+      />
     </>
   )
 }
@@ -14,42 +24,46 @@ const Total = (props) => {
   console.log(props)
   return (
     <p>
-      Number of exercises{' '}
-      {props.exercise1 + props.exercise2 + props.exercise3}
+      Number of exercises: {props.exercise1 + props.exercise2 + props.exercise3}
     </p>
   )
 }
 
 const Part = (props) => {
   return (
-    <p>{props.part}: {props.exercise} </p>
+    <p>
+      {props.part}: {props.exercise}{' '}
+    </p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content
         part1={part1}
-        exercise1={exercises1}
         part2={part2}
-        exercise2={exercises2}
         part3={part3}
-        exercise3={exercises3}
       />
       <Total
-        exercise1={exercises1}
-        exercise2={exercises2}
-        exercise3={exercises3}
+        exercise1={part1.exercises}
+        exercise2={part2.exercises}
+        exercise3={part3.exercises}
       />
     </div>
   )
