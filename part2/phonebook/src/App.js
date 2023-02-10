a7ad4a807628fa7576d9650ad8f5fa84724debcd
Bruno Vidal <brunokasprik@gmail.com>
2023-02-10 03:16:00 -0300

    exerc 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 0 }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    setPersons([...persons, { name: newName, id: persons.length++ }])
+    setNewName('')
+    
+  }
+  const handleNewNameChange = (event) => {
+    // console.log(event) 
+    setNewName(event.target.value)
+    
+
+  }
+  console.log(persons)
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNewNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.id}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
