4464959c2190fa090db20bdadd338d5e17298f2a
Bruno Vidal <brunokasprik@gmail.com>
2023-02-11 02:22:06 -0300

    exerc 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,18 @@
 import Persons from './Components/Persons'
 import Form from './Components/Form'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './Components/Filter'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 0 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
-
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+         .then((r) => setPersons(r.data))
+  }, [])
   // console.log(fPersons)
   // console.log(persons)
 
