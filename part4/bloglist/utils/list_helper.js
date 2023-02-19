3dbe07aa29ba6c7df391f47f5135390e5420bfa2
Bruno Vidal <brunokasprik@gmail.com>
2023-02-18 22:57:12 -0300

    exerc 4.5 step 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -5,8 +5,15 @@ function dummy(blogs) {
 function totalLikes(blogs) {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+function favoriteBlog(blogs) {
+  return blogs.reduce(
+    (favorite, blog) => (blog.likes > favorite.likes ? blog : favorite),
+    blogs[0]
+  )
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 }
