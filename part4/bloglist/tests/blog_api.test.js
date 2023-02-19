ab740dc9e0ef29fcf8a41382362b77370be1a7ae
Bruno Vidal <brunokasprik@gmail.com>
2023-02-19 04:46:30 -0300

    exerc 4.11 step 4

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog))
+  const promiseArray = blogObjects.map((blog) => blog.save())
+  await Promise.all(promiseArray)
+})
 
 describe('Correct format and amount of blogs', () => {
   test('blogs are returned as json', async () => {
@@ -46,6 +55,23 @@ describe('Blogs POST ', () => {
 
     expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
     expect(title).toContain('Cookies')
+  })
+})
+describe('Likes test', () => {
+  test('likes default to 0 if missing', async () => {
+    const newBlog = {
+      title: 'Milk',
+      author: 'Cow',
+      url: 'https://milk.com',
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.likes).toBe(0)
   })
 })
 
